24717b630beaa189e263d427a745521777532e1e
xkjiang <jiang_xk@sina.cn>
2017-02-22 23:00:45 +0800

    init

---
+++ aspectjplugin/src/main/groovy/com/jiang/aspectjplugin/Aspectj.groovy
@@ -0,0 +1,53 @@
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+public class Aspectj implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.dependencies {
+            'org.aspectj:aspectjrt:1.8.9'
+        }
+        final def log = project.logger
+        log.error("=================================")
+        log.error("Aspectj 开始注入代码到目标class文件")
+        log.error("==================================")
+        android.libraryVariants.all { variant ->
+            JavaCompile javaCompile = variant.javaCompile
+            javaCompile.doLast {
+                String[] args = ["-showWeaveInfo",
+                                 "-1.8",
+                                 "-inpath", javaCompile.destinationDir.toString(),
+                                 "-aspectpath", javaCompile.classpath.asPath,
+                                 "-d", javaCompile.destinationDir.toString(),
+                                 "-classpath", javaCompile.classpath.asPath,
+                                 "-bootclasspath", plugin.project.android.bootClasspath.join(
+                        File.pathSeparator)]
+
+                MessageHandler handler = new MessageHandler(true);
+                new Main().run(args, handler)
+
+                for (IMessage message : handler.getMessages(null, true)) {
+                    switch (message.getKind()) {
+                        case IMessage.ABORT:
+                        case IMessage.ERROR:
+                        case IMessage.FAIL:
+                            log.error message.message, message.thrown
+                            break;
+                        case IMessage.WARNING:
+                        case IMessage.INFO:
+                            log.info message.message, message.thrown
+                            break;
+                        case IMessage.DEBUG:
+                            log.debug message.message, message.thrown
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
